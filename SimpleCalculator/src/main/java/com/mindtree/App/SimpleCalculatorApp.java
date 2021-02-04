73563b207e00686b64a69c90b0973aed9daaf35b
vimaleshn98 <vimaleshn98@gmail.com>
2021-02-04 11:37:54 +0530

    Created Addition Feature

---
+++ SimpleCalculator/src/main/java/com/mindtree/App/SimpleCalculatorApp.java
@@ -0,0 +1,31 @@
+package com.mindtree.App;
+
+import java.util.Scanner;
+
+public class SimpleCalculatorApp {
+	static Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int choice;
+		boolean flag;
+		do {
+			System.out.println(
+					"1 --> Add two number\n 2 --> Substract two number\n 3--> Multiply Number\n Enter your choice");
+			choice = scanner.nextInt();
+			switch (choice) {
+			case 1:
+				System.out.println("Addition of number" + (2+5));
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				
+				break;
+			default:
+				System.out.println("-----------------Existing Main Menu-------------------");
+			}
+		} while (choice <= 3 && choice >= 1);
+	}
+
+}
